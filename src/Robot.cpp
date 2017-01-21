8f947a715cc1a612343862a81a5619d6d9ea4248
RobotBoy23 <ishaan.sawh@gmail.com>
2017-01-20 20:43:56 -0500

    Add files via upload

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <memory>
 #include <string>
-
+#include <CANTalon.h>
+#include <RobotDrive.h>
+#include <WPILib.h>
+#include <Joystick.h>
 #include <IterativeRobot.h>
 #include <LiveWindow/LiveWindow.h>
 #include <SmartDashboard/SendableChooser.h>
 #include <SmartDashboard/SmartDashboard.h>
-#include <Joystick.h>
-#include <CANTalon.h>
 #include <AHRS.h>
 
 class Robot: public frc::IterativeRobot {
@@ -17,13 +18,18 @@ public:
 		chooser.AddObject(autoNameCustom, autoNameCustom);
 		frc::SmartDashboard::PutData("Auto Modes", &chooser);
 
-		PTOMotor1 = new CANTalon(1);
-		PTOMotor2 = new CANTalon(2);
+		frontLeftMotor = new CANTalon(1);
+		frontRightMotor = new CANTalon(2);
+		backLeftMotor = new CANTalon(3);
+		backRightMotor = new CANTalon(4);
 
-		stick = new Joystick(0);
+		myDrive = new RobotDrive(frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor);
 
-		Button6 = new JoystickButton(stick, 6);
-		intakeMotor = new CANTalon(1);
+		controller = new Joystick(0);
+
+
+		ahrs = new AHRS(SPI::Port::kMXP);
+
 
 
 	}
@@ -34,7 +40,7 @@ public:
 	 * chooser code works with the Java SmartDashboard. If you prefer the
 	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
 	 * GetString line to get the auto name from the text box below the Gyro.
-	 *
+	 *`	1`
 	 * You can add additional auto modes by adding additional comparisons to the
 	 * if-else structure below with additional strings. If using the
 	 * SendableChooser make sure to add them to the chooser code above as well.
@@ -60,18 +66,19 @@ public:
 	}
 
 	void TeleopInit() {
+		SmartDashboard::PutNumber(  "IMU_TotalYaw", ahrs->GetAngle());
+		SmartDashboard::PutNumber(  "IMU_Accel_X",          ahrs->GetWorldLinearAccelX());
+		SmartDashboard::PutNumber(  "IMU_Accel_Y",          ahrs->GetWorldLinearAccelY());
 
 	}
 
 	void TeleopPeriodic() {
-
-		if(Button6){
-			intakeMotor->Set(1);
-		}else{
-			intakeMotor->Set(0);
+		myDrive->TankDrive(controller->GetRawAxis(1), controller->GetRawAxis(3), true);
 
 
-		}
+
+
+
 
 
 	}
@@ -81,21 +88,19 @@ public:
 	}
 
 private:
-
 	frc::LiveWindow* lw = LiveWindow::GetInstance();
 	frc::SendableChooser<std::string> chooser;
 	const std::string autoNameDefault = "Default";
 	const std::string autoNameCustom = "My Auto";
 	std::string autoSelected;
-	Joystick* stick;
-	CANTalon* PTOMotor1;
-	CANTalon* PTOMotor2;
+	CANTalon *frontLeftMotor;
+	CANTalon *frontRightMotor;
+	CANTalon *backLeftMotor;
+	CANTalon *backRightMotor;
+	RobotDrive *myDrive;
+	frc::Joystick* controller;
+	AHRS *ahrs;
 
-
-
-
-	JoystickButton* Button6;
-
-
-	CANTalon* intakeMotor;
 };
+
+START_ROBOT_CLASS(Robot)
